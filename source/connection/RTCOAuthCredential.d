2bf64844710bf0f83b869e89159b97ae86e89760
Gyorgy Katona <gykatona@logmein.com>
2018-09-04 11:28:47 +0200

    rtcoauthcredential

---
+++ source/connection/RTCOAuthCredential.d
@@ -0,0 +1,9 @@
+module connection.oauth;
+
+/**
+ *  Describe OAuth credential information
+ */
+struct RTCOAuthCredential {
+    wstring _macKey;        /// base-64 encoded
+    wstring _accessToken;   /// base-64 encoded
+}
